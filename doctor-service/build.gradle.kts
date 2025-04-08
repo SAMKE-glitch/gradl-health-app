ae5f69df102f64f926ff65ff122539ce35f1d8eb
samke-glitch <sammythemwa@gmail.com>
2025-04-08 10:24:33 +0300

    new: doctorService

--- doctor-service/build.gradle.kts
+++ doctor-service/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":common-proto"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
